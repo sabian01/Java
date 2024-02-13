8781dd71e20afe01876f9471438df8a37e00ce14
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-02-13 16:53:41 +0100

    Create Ushtrimi6.java

---
+++ 2.Variablat dhe Data Types/Ushtrimi6.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+class Main {//deklarimi i klases
+
+     public static void main (String [] args){ //deklarimi i metodes kryesore
+         Date date = new Date();
+
+         System.out.println("Data aktuale: " +date);
+     }
