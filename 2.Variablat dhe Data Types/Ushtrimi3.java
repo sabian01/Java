6087901d337a8f6928115888661edea0dedd8d37
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-02-13 16:35:21 +0100

    Update Ushtrimi3.java

--- 2.Variablat dhe Data Types/Ushtrimi3.java
+++ 2.Variablat dhe Data Types/Ushtrimi3.java
@@ -1,1 +1,15 @@
-sdf
+
+class Main {//deklarimi i klases
+
+     public static void main (String [] args){ //deklarimi i metodes kryesore
+
+         int a = 10;
+         int b = 2;
+
+         int pjestimi = a / 2;
+
+         System.out.println(a +" / " +b +" = " +pjestimi);
+
+     }
+
+}
