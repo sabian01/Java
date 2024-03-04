b9d0f44d1489cd0f2f0e2d970ee5bd2f940a7744
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-04 23:54:03 +0100

    Create ushtrimi2.java

---
+++ 5.Arrays/ushtrimi2.java
@@ -0,0 +1,13 @@
+    public static void main(String[] args) {
+
+        String[] ngjyrat =  {
+                "Blu",
+                "Jeshile",
+                "E kuqe",
+                "E bardhe",
+                "E verdhe"
+        };
+
+        System.out.println(Arrays.toString(ngjyrat));
+
+    }
