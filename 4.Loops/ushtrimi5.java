8959571257a3f323fe2a533ef7245c23b81868b2
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-04 23:27:22 +0100

    Create ushtrimi5.java

---
+++ 4.Loops/ushtrimi5.java
@@ -0,0 +1,12 @@
+ public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 30; i++){
+            if (i % 3 == 0){
+                System.out.println(i);
+            }
+          
+        }
+
+
+    }
