ee81fcba77071eb14459e7c296d6642d8804b355
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-04 23:56:16 +0100

    Create ushtrimi3.java

---
+++ 5.Arrays/ushtrimi3.java
@@ -0,0 +1,12 @@
+ public static void main(String[] args) {
+
+        int[] numrat =  {4, 7, 3, 2, 1};
+
+
+        System.out.println(Arrays.toString(numrat));
+
+        Arrays.sort(numrat);
+
+        System.out.println(Arrays.toString(numrat));
+        
+    }
