0737ea2440fe86b0b7698d810a97d9e27243bb1b
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-04 23:21:59 +0100

    Create ushtrimi2.java

---
+++ 4.Loops/ushtrimi2.java
@@ -0,0 +1,17 @@
+ public static void main(String[] args) {
+
+        //Ushtrimi 1
+
+        //Inicializojme variablen shuma me vleren 0
+        int shuma = 0;
+
+        for (int i = 1; i <= 10; i++){
+            //per cdo iterim te loopit for
+            //variabla shuma do te rritet me vleren e variables "i"
+            shuma = shuma + i;
+        }
+
+        System.out.println("Shuma eshte " +shuma);
+
+
+    }
