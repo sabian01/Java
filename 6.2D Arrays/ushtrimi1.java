bd0696a61f41134672acb1e48912b34a078d385f
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-05 21:00:25 +0100

    Create ushtrimi1.java

---
+++ 6.2D Arrays/ushtrimi1.java
@@ -0,0 +1,18 @@
+ public static void main (String [] args) {
+          Scanner scanner = new Scanner(System.in);
+
+          //Ushtrimi 1
+
+          String[][] yje =  {
+
+                  {"*", "*", "*"},
+
+                  {"*", "*", "*"},
+
+                  {"*", "*", "*"}
+          };
+
+
+          System.out.println(Arrays.deepToString(yje));
+          
+     }
