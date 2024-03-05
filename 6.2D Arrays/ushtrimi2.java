62d5a8c9a491c1d6fdd9cd21abf6384a8a747ed4
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-05 21:03:24 +0100

    Create ushtrimi2.java

---
+++ 6.2D Arrays/ushtrimi2.java
@@ -0,0 +1,18 @@
+ public static void main (String [] args) {
+          Scanner scanner = new Scanner(System.in);
+
+          //Ushtrimi 2
+
+          int[][] numra =  {
+                  {1, 2, 3, 4},
+
+                  {5, 6, 7, 8},
+
+                  {9, 10, 11, 12},
+
+                  {13, 14, 15, 16}
+          };
+
+          System.out.println(Arrays.deepToString(numra));
+
+     }
