0826a87878b11c680d60321df324397626a580fc
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-06 12:46:05 +0100

    Create ushtrimi7.java

---
+++ 6.2D Arrays/ushtrimi7.java
@@ -0,0 +1,52 @@
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Jep numrin e rreshtave: ");
+        int rreshta = scanner.nextInt();
+
+        System.out.println("Jep numrin e kolonave: ");
+        int kolona = scanner.nextInt();
+
+
+        int[][] numra = new int[rreshta][kolona];
+
+
+        int shuma = 0;
+        int[] shumaPerRresht = new int[rreshta];
+        double[] mesPerRresht = new double[rreshta];
+        int vlMax = numra[0][0];
+        int vlMin = 1;
+
+        for (int r = 0; r < rreshta; r++){
+            System.out.println("Jep elementet e rreshtit " +r);
+            for (int c = 0; c < kolona; c++){
+                numra[r][c] = scanner.nextInt();
+                shuma = shuma + numra[r][c];
+                shumaPerRresht[r] = shumaPerRresht[r] + numra[r][c];
+
+                if (vlMin > numra[r][c]){
+                    vlMin = numra[r][c];
+                }
+
+                if (vlMax < numra[r][c]){
+                    vlMax = numra[r][c];
+                }
+            }
+            mesPerRresht[r] = shumaPerRresht[r]/numra[r].length;
+        }
+
+
+        System.out.println(Arrays.deepToString(numra));
+
+        System.out.println("Shuma totale e elementeve eshte : " +shuma);
+
+        for (int r = 0; r < rreshta; r++){
+
+            System.out.println("Mesatarja , rreshti " +r +" = " +mesPerRresht[r]);
+        }
+
+
+        System.out.println("Vlera Max = " +vlMax);
+        System.out.println("Vlera Min = " +vlMin);
+
+    }
