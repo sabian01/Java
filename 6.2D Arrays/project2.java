8ec5e39ac0bf0407552b33c93ae5762867b3b835
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-06 12:51:14 +0100

    Update project2.java

--- 6.2D Arrays/project2.java
+++ 6.2D Arrays/project2.java
@@ -1,24 +1,7 @@
 public static void main (String [] args) {
           Scanner scanner = new Scanner(System.in);
 
-          //Program 2 pastaj , vazhdo me project 1 kalkulo
-          //xhiro ditore me te ulet per kamarier
-          /**Project 2
-           * -- Bar & Restorant --
-           * Nje restorant ka 3 kamariere, Krijo nje program ne java
-           * i cili kalkulon shumen totale te xhiros javore dhe vleren
-           * mesatare per xhiro ne dite per secilin nga kamarieret.
-           * Ne fund progami duhet te gjej kush eshte kamarieri me efficent.
-           *
-           * Xhiro per kamarier:
-           *
-           * Kamarieri 1:  75,  200 ,90, 125, 60, 140, 80
-           *
-           * Kamarieri 2: 170, 130, 400,  90, 120 , 315, 600
-           *
-           * Kamarieri 3: 100, 95, 320, 115, 120, 98, 250
-           * */
-
+         
           int[][] kmr = {
                   {75, 200, 90, 125, 60, 140, 80},
 
