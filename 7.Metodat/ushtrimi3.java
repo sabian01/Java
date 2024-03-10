98f1a8292fc146f2879b017df4fcc9750b8d2fa2
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-10 20:53:29 +0100

    Create ushtrimi3.java

---
+++ 7.Metodat/ushtrimi3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        printNr();
+    }
+
+    static void printNr () {
+        for (int i = 1; i <= 10; i++){
+            System.out.print(i +" ");
+        }
+    }
+
+
+}
