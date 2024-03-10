be898a2edc6a6ffbe946151c30d72d484972026c
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-10 20:55:08 +0100

    Create ushtrimi4.java

---
+++ 7.Metodat/ushtrimi4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        siperfaqe(4, 10);
+    }
+
+
+    static void siperfaqe(int a, int b) {
+        System.out.println("Siperfaqia e drejtkendeshit = " +a*b);
+    }
+
+
+}
