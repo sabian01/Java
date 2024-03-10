23df8f5c7c9a59e687f225e5397759743d6e0b1b
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-10 21:04:19 +0100

    Create ushtrimi7.java

---
+++ 7.Metodat/ushtrimi7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Diferenca = " +diferenca(7, 3));
+    }
+    
+    static int diferenca (int a, int b) {
+        return a - b;
+    }
+
+
+
+}
+
+
+
+
+
