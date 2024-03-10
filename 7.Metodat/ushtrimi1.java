aa4833509fe0a43368137ecfa9305ac536c3bc94
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-10 20:49:51 +0100

    Create ushtrimi1.java

---
+++ 7.Metodat/ushtrimi1.java
@@ -0,0 +1,15 @@
+
+class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ckemi();
+    }
+    
+
+    static void ckemi () {
+        System.out.println("Ckemi!");
+    }
+
+}
