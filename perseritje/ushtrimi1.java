6d4bca7f9e177725d5542c0d6b42c65dbf951edd
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-13 12:09:12 +0100

    Create ushtrimi1.java

---
+++ perseritje/ushtrimi1.java
@@ -0,0 +1,8 @@
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int nr1 = scanner.nextInt();
+        int nr2 = scanner.nextInt();
+
+        System.out.println(nr1 +" x " +nr2 +" = " +nr1*nr2);
+    }
