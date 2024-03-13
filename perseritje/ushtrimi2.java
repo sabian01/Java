04c68265dd2338464e624e8c251bf14609a4300c
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-13 12:23:19 +0100

    Create ushtrimi2.java

---
+++ perseritje/ushtrimi2.java
@@ -0,0 +1,10 @@
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = 2;
+        int b = 2;
+        int c = 4;
+
+        System.out.println(a+b+c);
+
+    }
