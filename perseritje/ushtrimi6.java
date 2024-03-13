b082519957f65e623bc3a8e4637d61ffcd325a87
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-13 12:45:00 +0100

    Create ushtrimi6.java

---
+++ perseritje/ushtrimi6.java
@@ -0,0 +1,13 @@
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numra = {7, 2, 6, 12, 20};
+
+
+        for (int i = 0; i < numra.length; i++) {
+            if (numra[i] == 6) {
+                System.out.println("Indeksi i numrit 6 eshte: " + i);
+            }
+        }
+
+    }
