af222a52b9a12e3a263e0a933f2b1bfea1d0532f
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-17 09:38:19 +0100

    Create Main.java

---
+++ 8.oop/ushtrimi1/Main.java
@@ -0,0 +1,13 @@
+
+class Main {
+    
+    public static void main(String[] args) {
+     
+        Person p1 = new Person("Sabian", 22);
+        System.out.println(p1.emri +p1.mosha);
+
+        Person p2 = new Person("Person2", 30);
+        System.out.println(p2.emri +p2.mosha);
+
+    }
+}
