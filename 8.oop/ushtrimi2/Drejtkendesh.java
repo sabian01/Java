5114cd772f28572fde139b55f7e69c6e541fa148
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-17 10:51:27 +0100

    Create Drejtkendesh.java

---
+++ 8.oop/ushtrimi2/Drejtkendesh.java
@@ -0,0 +1,9 @@
+public class Drejtkendesh {
+    int gjeresi;
+    int lartesi;
+
+    Drejtkendesh(int gjeresi, int lartesi){
+        this.gjeresi = gjeresi;
+        this.lartesi = lartesi;
+    }
+}
