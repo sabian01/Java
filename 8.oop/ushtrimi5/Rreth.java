df64c2030c1bc855c2923c5f6f86ad977c40d51e
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-17 11:38:21 +0100

    Create Rreth.java

---
+++ 8.oop/ushtrimi5/Rreth.java
@@ -0,0 +1,17 @@
+
+public class Rreth {
+    int r;
+
+    //constructor
+    Rreth (int r) {
+        this.r = r;
+    }
+
+    public void printSp() {
+        System.out.println("Siperfaqja = " +Math.PI*(r*r));
+    }
+
+    public static void printStatic()  {
+        System.out.println("Une jame metode statike.");
+    }
+}
