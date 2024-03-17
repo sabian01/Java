7d433959a9888a0da648157cebd453b552a4c7ab
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-17 09:31:49 +0100

    Create Person.java

---
+++ 8.oop/ushtrimi1/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    //attributes
+    String emri;
+    int mosha;
+
+    //constructor
+    Person (String emri, int mosha) {
+        this.emri = emri;
+        this.mosha = mosha;
+    }
+    
+}
