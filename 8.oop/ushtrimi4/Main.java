f8cef5f6127db013d1dedd3fa754757d9c341024
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-17 11:35:36 +0100

    Create Main.java

---
+++ 8.oop/ushtrimi4/Main.java
@@ -0,0 +1,13 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        Rreth r1 = new Rreth(7);
+        Rreth r2 = new Rreth(12);
+
+        r1.printSp();
+        r2.printSp();
+
+    }
+
+}
