87761e656a73523d1cd74a134ec17ac153aad8c7
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-17 11:35:06 +0100

    Create Rreth.java

---
+++ 8.oop/ushtrimi4/Rreth.java
@@ -0,0 +1,12 @@
+public class Rreth {
+    int r;
+
+    //constructor
+    Rreth (int r) {
+        this.r = r;
+    }
+
+    public void printSp() {
+        System.out.println("Siperfaqja = " +Math.PI*(r*r));
+    }
+}
