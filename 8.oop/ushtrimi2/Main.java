dc171100211b80eb50f275fe74e858974fa7ff78
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-17 10:51:50 +0100

    Create Main.java

---
+++ 8.oop/ushtrimi2/Main.java
@@ -0,0 +1,13 @@
+class Main {
+
+    public static void main(String[] args) {
+        Drejtkendesh d1 = new Drejtkendesh(5, 7);
+        Drejtkendesh d2 =  new Drejtkendesh(4, 3);
+
+        System.out.println("Siperfaqja = " +(d1.lartesi*d1.gjeresi));
+        System.out.println("Siperfaqja = " +(d2.lartesi*d2.gjeresi));
+
+    }
+
+}
+
