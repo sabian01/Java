cf8a58eb2380edce6d4c4181bf1028aa6666eac3
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-20 12:18:16 +0100

    Create Main.java

---
+++ OOP Part2/ushtrimi3/Main.java
@@ -0,0 +1,12 @@
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Semafor s1 = new Semafor("Jeshil", 1);
+
+        s1.ndryshoNjgyren("E kuqe");
+
+        System.out.println(s1.shikoNgjyren());
+    }
+}
