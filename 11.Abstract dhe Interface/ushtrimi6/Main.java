c39b3faebc99a61593b8117107a3677cf8fb0c92
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 20:46:03 +0100

    Create Main.java

---
+++ 11.Abstract dhe Interface/ushtrimi6/Main.java
@@ -0,0 +1,13 @@
+class Main {
+
+    public static void main(String[] args) {
+        CallOfDuty cf = new CallOfDuty();
+        GTA gta = new GTA();
+        FIFA fifa = new FIFA();
+        cf.play();
+        gta.play();
+        cf.play();
+
+    }
+
+}
