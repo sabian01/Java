c5a9fc7ad753c8d100e3dec63aa85abe7a29cc73
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 19:32:42 +0100

    Create Main.java

---
+++ 11.Abstract dhe Interface/ushtrimi4/Main.java
@@ -0,0 +1,31 @@
+
+class Main {
+
+    public static void main(String[] args) {
+        Drejtkendesh d1 = new Drejtkendesh(3, 5);
+        d1.draw();
+        d1.resize();
+        System.out.println();
+        d1.draw();
+
+        System.out.println();
+
+        Katrore k1 = new Katrore(6);
+        k1.draw();
+        k1.resize();
+        System.out.println();
+        k1.draw();
+
+
+        //Ushtrimi 4
+        d1.siperfaqe();
+        d1.perimeter();
+
+        k1.siperfaqe();
+        k1.perimeter();
+    }
+
+}
+
+
+
