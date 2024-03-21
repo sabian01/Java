e281ac3da7bbaaba3d8836a9bc4f63343a26cd91
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 12:50:08 +0100

    Create Dhelpra.java

---
+++ 11.Abstract dhe Interface/Interface shembull/Dhelpra.java
@@ -0,0 +1,12 @@
+public class Dhelpra implements Gjuetari, Gjah{
+
+    @Override
+    public void vrapo() {
+        System.out.println("Dhelpra po vrapon");
+    }
+
+    @Override
+    public void gjuaj() {
+        System.out.println("Dhelpra po gjuan");
+    }
+}
