853e54172dbe9118e2e50241406e2c56631cacdc
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 13:25:16 +0100

    Create Main.java

---
+++ 11.Abstract dhe Interface/ushtrimi1/Main.java
@@ -0,0 +1,13 @@
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Akrep a1 = new Akrep();
+        a1.habitati();
+
+        Delfin d1 = new Delfin();
+        d1.habitati();
+    }
+}
+
