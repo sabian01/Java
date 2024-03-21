4af8eaf2108a251a61941532f18f7a8a03134359
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 19:31:16 +0100

    Create Gjeometrik.java

---
+++ 11.Abstract dhe Interface/ushtrimi4/Gjeometrik.java
@@ -0,0 +1,14 @@
+
+public abstract class Gjeometrik {
+
+    public abstract void draw();
+
+    public abstract void resize();
+
+    public abstract void siperfaqe();
+
+    public abstract void perimeter();
+
+
+
+}
