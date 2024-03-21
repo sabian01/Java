14799dfdfe5697b21ff04d4c0915f9db3d66c445
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 13:01:11 +0100

    Create Main.java

---
+++ 11.Abstract dhe Interface/Abstract shembull/Main.java
@@ -0,0 +1,18 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        //Instanca e klases Makina
+        Makina mk1 = new Makina();
+        mk1.drive();
+        mk1.stop();
+
+        //Instanca e klases Moto
+        Moto mt1 = new Moto();
+        mt1.drive();
+        mt1.stop();
+
+
+
+    }
+}
