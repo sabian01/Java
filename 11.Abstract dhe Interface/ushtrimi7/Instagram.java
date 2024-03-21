31717221da262ae39651f07a36fc1c75e7191dd0
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 20:52:41 +0100

    Create Instagram.java

---
+++ 11.Abstract dhe Interface/ushtrimi7/Instagram.java
@@ -0,0 +1,11 @@
+public class Instagram implements App{
+    @Override
+    public void login() {
+        System.out.println("Ju u loguat ne instagram");
+    }
+
+    @Override
+    public void logout() {
+        System.out.println("Ju dolet nga llogaria e instagramit.");
+    }
+}
