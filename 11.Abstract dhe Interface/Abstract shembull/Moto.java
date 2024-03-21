e1652aa80231b36ac42b124577dc050d22553bef
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 13:00:37 +0100

    Create Moto.java

---
+++ 11.Abstract dhe Interface/Abstract shembull/Moto.java
@@ -0,0 +1,11 @@
+public class Moto extends Mjet{
+    @Override
+    public void drive() {
+        System.out.println("Motorri po ecen.");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Motorri ndaloi.");
+    }
+}
