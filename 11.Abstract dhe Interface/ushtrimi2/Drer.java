bc3d696a60fcc36eed74c5b651d82c9b7493adc1
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 14:40:08 +0100

    Create Drer.java

---
+++ 11.Abstract dhe Interface/ushtrimi2/Drer.java
@@ -0,0 +1,12 @@
+public class Drer extends Animal{
+
+    @Override
+    public void eat() {
+        System.out.println("Dreri ushqehet me bimore.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Dreri fle.");
+    }
+}
