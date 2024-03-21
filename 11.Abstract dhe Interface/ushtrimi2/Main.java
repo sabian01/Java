774039733fa6c6c80fccdb1afa74ed903b5204a7
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 14:40:29 +0100

    Create Main.java

---
+++ 11.Abstract dhe Interface/ushtrimi2/Main.java
@@ -0,0 +1,20 @@
+class Main {
+
+    public static void main(String[] args) {
+
+      Luan l1 = new Luan();
+      l1.eat();
+      l1.sleep();
+
+
+      Tiger t1 = new Tiger();
+      t1.eat();
+      t1.sleep();
+
+      Drer d1 = new Drer();
+      d1.eat();
+      d1.sleep();
+
+
+    }
+}
