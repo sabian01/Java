ffae0ca821fed8554fc5877aed17df1e463ca499
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 14:39:28 +0100

    Create Luan.java

---
+++ 11.Abstract dhe Interface/ushtrimi2/Luan.java
@@ -0,0 +1,12 @@
+public class Luan extends Animal{
+
+    @Override
+    public void eat() {
+        System.out.println("Luani ha gjahun e tij.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Luani fle.");
+    }
+}
