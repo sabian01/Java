ac7863f77fd9d9a2a11ed95c3e3ebc8fe2819878
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 12:50:38 +0100

    Create Main.java

---
+++ 11.Abstract dhe Interface/Interface shembull/Main.java
@@ -0,0 +1,17 @@
+class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+       Luan l1 = new Luan();
+       l1.gjuaj();
+
+       Lepur lp1 = new Lepur();
+       lp1.vrapo();
+
+       Dhelpra d1 = new Dhelpra();
+       d1.gjuaj();
+       d1.vrapo();
+
+    }
+}
