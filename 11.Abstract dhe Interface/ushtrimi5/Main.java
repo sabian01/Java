16f8d3f14aeea09f658153abad5ab4586351468f
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 19:56:49 +0100

    Create Main.java

---
+++ 11.Abstract dhe Interface/ushtrimi5/Main.java
@@ -0,0 +1,17 @@
+class Main {
+
+    public static void main(String[] args) {
+        Peshk peshk = new Peshk();
+        peshk.noto();
+
+        Peshkaqen peshkaqen = new Peshkaqen();
+        peshkaqen.gjuaj();
+
+        Oktapod oktapod = new Oktapod();
+        oktapod.gjuaj();
+
+        Gaforre gaforre = new Gaforre();
+        gaforre.noto();
+    }
+
+}
