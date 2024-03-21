c9da8d2d40dd4ee19a4d373bf9cc8e1fce3dc9a0
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 13:00:18 +0100

    Create Makina.java

---
+++ 11.Abstract dhe Interface/Abstract shembull/Makina.java
@@ -0,0 +1,11 @@
+public class Makina extends Mjet{
+    @Override
+    public void drive() {
+        System.out.println("Makina po ecen.");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Makina ndaloi.");
+    }
+}
