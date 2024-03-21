7c26eb63de73fbd4c2399d7c9c784fd549101b07
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-21 14:39:46 +0100

    Create Tiger.java

---
+++ 11.Abstract dhe Interface/ushtrimi2/Tiger.java
@@ -0,0 +1,12 @@
+public class Tiger extends Animal{
+
+    @Override
+    public void eat() {
+        System.out.println("Tigri ha gjahun e tij.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Tigri fle.");
+    }
+}
