f94a5bbed6a49fbc0632af3fa0fcf5f2c703793c
sabian01 <149004562+sabian01@users.noreply.github.com>
2024-03-22 21:13:07 +0100

    Create Menu.java

---
+++ projekt/Menu.java
@@ -0,0 +1,20 @@
+public class Menu {
+    //Fields
+    private String artikull;
+    private int cmimi;
+
+    //Konstruktor
+    public Menu (String artikull, int cmimi) {
+        this.artikull = artikull;
+        this.cmimi = cmimi;
+    }
+
+    public String getArtikull() {
+        return artikull;
+    }
+
+    public int getCmimi() {
+        return cmimi;
+    }
+
+}
